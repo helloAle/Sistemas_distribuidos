f9eb8b0d71b7e5e5851715d7b0a3f5cf80d137d6
Alexandre Oliveira <89224363+helloAle@users.noreply.github.com>
2023-04-19 20:22:44 -0300

    atividade01

--- semana08/atividade01.py
+++ semana08/atividade01.py
@@ -1,7 +1,21 @@
 from threading import Thread
 
-for i in range(1,501):
-    print(i)
+def ordem_crescente():
+    for i in range(1,501):
+        print(i)
 
-for j in range(500,0,-1):
-    print(j)
+def ordem_decrescente():
+    for j in range(500,0,-1):
+        print(j)
+
+
+if __name__ == '__main__':
+
+    t1=Thread(target= ordem_crescente)
+    t2=Thread(target= ordem_decrescente)
+
+t1.start()
+t1.join()
+
+t2.start()
+t2.join()
